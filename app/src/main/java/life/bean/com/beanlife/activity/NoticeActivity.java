a27ecd86a37ceab1a4ea65bef18f3b72ad389444
13608676132@163.com <13608676132@163.com>
2017-02-28 17:09:51 +0800

    通知的布局  1707

---
+++ app/src/main/java/life/bean/com/beanlife/activity/NoticeActivity.java
@@ -0,0 +1,24 @@
+package life.bean.com.beanlife.activity;
+
+import life.bean.com.beanlife.R;
+
+/**
+ * 作者 : bean on 2017/2/27/0027.
+ * 注释 :
+ */
+public class NoticeActivity extends BaseActivity{
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_notice;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
